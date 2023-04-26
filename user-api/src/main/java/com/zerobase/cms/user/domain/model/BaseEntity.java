3abafa6877d53655876fdf7b370eda70c0f182c7
Ogu1208 <kdasunb@naver.com>
2023-04-27 01:53:46 +0900

    feat : [002-SIGN-UP] 커스터머(고객)에 대한 Signup Service 구현

---
+++ user-api/src/main/java/com/zerobase/cms/user/domain/model/BaseEntity.java
@@ -0,0 +1,20 @@
+package com.zerobase.cms.user.domain.model;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDate;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(value = {AuditingEntityListener.class})
+public class BaseEntity {
+    @CreatedDate
+    private LocalDate createdAt;
+    @LastModifiedDate
+    private LocalDate modifiedAt;
+}
